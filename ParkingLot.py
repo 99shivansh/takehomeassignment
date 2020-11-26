d88bd4b89d68aa628ac5a218c9459bf13f3b17e1
99shivansh <shivansh8sharma@gmail.com>
2020-11-26 16:16:09 +0530

    initial commit

---
+++ ParkingLot.py
@@ -0,0 +1,168 @@
+
+
+import heapq
+
+
+class Parkinglot:
+  
+#__init__:
+#constructor is used to initialise parkingLotCapacity to -1.
+# vacantSlots is initialized to an empty list.
+# For each slot, we will be storing vehicleNumber & driverAge. As there
+#can be maximum 1000 slots, we have taken a 2D list.
+# For every possible age of driver, we maintain a list which stores slot &
+#vehicleNumber. As the maximum age can be 1000, we need a 2D list.
+# We need a map (dict) to store slots against vehicle number.
+
+
+
+    def __init__(self):
+        self.CapacityparkingLot = -1
+        self.slotsVacant = []
+        self.slots = [[] for i in range(1001)]
+        self.agesofdriver = [[] for i in range(1001)]
+        self.vehicleslotmap = {}
+
+
+#Capacity():
+# parkingLotCapacity is initialized to ​ cap ​ in the function.
+# vacantSlots is filled with slot numbers from 1 to ​ cap . ​
+# Min heap is created for vacantSlots.
+
+
+
+    def Capacity(self, CapacityparkingLot):
+        self.CapacityparkingLot = CapacityparkingLot
+        for i in range(1, self.CapacityparkingLot + 1):
+            self.slotsVacant.append(i)
+        heapq.heapify(self.slotsVacant)
+        return "Created parking of {} slots".format(CapacityparkingLot)
+
+#Vehiclesparking(driverAge, vehicleNo):
+#The nearest vacant slot is popped from the vacantSlots heap.
+#[slot, ​ vehicleNo ​ ] is stored for ​ driverAge ​ .
+#[​ vehicleNo ​ , ​ driverAge ] ​ is mapped to the available slot.
+#vehicleNo ​ is mapped to the available slot.
+
+
+
+    def Vehiclesparking(self, driverAge, vehicleNumber):
+        if self.CapacityparkingLot < 0:
+            return "Parking lot space is not allocated yet!."
+
+        if len(self.slotsVacant) == 0:
+            return "Cannot park more vehicles.Sorry"
+
+        availableSlot = heapq.heappop(self.slotsVacant)
+        self.agesofdriver[driverAge].append([availableSlot, vehicleNumber])
+        self.slots[availableSlot] = [vehicleNumber, driverAge]
+        self.vehicleslotmap[vehicleNumber] = availableSlot
+
+        return "Car with vehicle registration number {} has been parked at slot number {}".format(vehicleNumber, availableSlot)
+
+
+#getSlotsWithAgeofDriver(driverAge):
+# Information is retrieved from ​ driverAge ​ mapping.
+
+
+
+    def getSlotsWithAgeofDriver(self, driverAge):
+        slots = []
+        for slot, vehicleNumber in self.agesofdriver[driverAge]:
+            slots.append(str(slot))
+        return ','.join(slots)
+
+
+#getSlotsVehicleNumber(vehicleNo):
+# Information about the slot is retrieved from ​ slotVehicleMap ​ .
+
+
+    def getSlotsVehicleNumber(self, vehicleNumber):
+        return self.vehicleslotmap.get(vehicleNumber, "No vehicle is parked with these details.")
+
+    
+#vacateSlot(slot):
+# if ​ slot ​ is valid & not empty
+# driverAge ​ & ​ vehicleNo ​ is retrieved from ​ slots ​ mapping.
+# mapping against ​ driverAge ​ is erased from ​ driverAges ​ .
+# mapping against ​ vehicleNo ​ is erased from ​ slotvehicleMap ​ .
+# mapping against ​ slot ​ is erased from ​ slots ​ .
+# slot ​ is made available to min heap ​ vacantSlots ​ .
+
+
+    def vacateSlot(self, slot):
+        if slot < 1 or slot > self.CapacityparkingLot:
+            return "There is no such slot. Are you sure?"
+
+        if self.slots[slot] == []:
+            return "Slot is already vacant."
+
+        vehicleNumber, driverAge = self.slots[slot]
+
+        self.slots[slot] = []
+
+        for i in range(len(self.agesofdriver[driverAge])):
+            if self.agesofdriver[driverAge][i][0] == slot:
+                self.agesofdriver[driverAge].pop(i)
+                break
+
+        self.vehicleslotmap.pop(vehicleNumber)
+
+        heapq.heappush(self.slotsVacant, slot)
+
+        return "Slot number {} vacated, the car with vehicle registration number {} left the space, the driver of the car was of age {}".format(slot, vehicleNumber, driverAge)
+
+ 
+#getCardetailsWithDriverAge(driverAge):
+#Information of cars is retrieved from ​ driverAges ​ mapping.
+
+
+    def getCardetailsWithDriverAge(self, driverAge):
+        cars = []
+        for slot, vehicleNumber in self.agesofdriver[driverAge]:
+            cars.append(vehicleNumber)
+        return ','.join(cars)
+
+
+def main():
+    parkingLot = Parkinglot()
+    input = open("./input.txt")
+
+    for line in input:
+        line = line.lstrip().rstrip()
+        try:
+            command = line.split()
+            action = command[0]
+
+            if action == "Create_parking_lot":
+                print(parkingLot.Capacity(int(command[1])))
+
+            elif action == "Park" and command[2] == "driver_age":
+                vehicleNumber = command[1]
+                driverAge = int(command[3])
+                print(parkingLot.Vehiclesparking(driverAge, vehicleNumber))
+
+            elif action == "Slot_numbers_for_driver_of_age":
+                driverAge = int(command[1])
+                print(parkingLot.getSlotsWithAgeofDriver(driverAge))
+
+            elif action == "Leave":
+                slot = int(command[1])
+                print(parkingLot.vacateSlot(slot))
+
+            elif action == "Slot_number_for_car_with_number":
+                vehicleNumber = command[1]
+                print(parkingLot.getSlotsVehicleNumber(vehicleNumber))
+
+            elif action == "Vehicle_registration_number_for_driver_of_age":
+                driverAge = int(command[1])
+                print(parkingLot.getCardetailsWithDriverAge(driverAge))
+
+            else:
+                print("Command Not Found Please use correct keywords.")
+        except:
+            print('Something went wrong with command -> ' + line)
+
+
+if __name__ == "__main__":
+    main()
---
+++ README.md
@@ -0,0 +1,111 @@
+<h1>Parking Lot Software</h1>
+
+<h2>Problem Statement</h2>
+We own a parking lot that can hold up to ‘n’ cars at any given point in time. Each slot is given a number starting at one increasing with increasing distance from the entry point in steps of one. We want to create an automated ticketing system that allows our customers to use our parking lot without human intervention.
+
+When a car enters the parking lot, we want to have a ticket issued to the driver. The ticket issuing process includes:- 
+1. We are taking note of the number written on the vehicle registration plate and the age of the driver of the car.
+2. And we are allocating an available parking slot to the car before actually handing over a ticket to the driver (we assume that our customers are kind enough to always park in the slots allocated to them).
+
+The customer should be allocated a parking slot that is nearest to the entry. At the exit, the customer returns the ticket, marking the slot they were using as being available.
+Due to government regulation, the system should provide us with the ability to find out:-
+- Vehicle Registration numbers for all cars which are parked by the driver of a certain age,
+- Slot number in which a car with a given vehicle registration plate is parked.
+- Slot numbers of all slots where cars of drivers of a particular age are parked.
+
+We interact with the system via a file-based input system, i.e. it should accept a filename as an input. The file referenced by filename will contain a set of commands separated by a newline, we need to execute the commands in order and produce output.
+
+<h3>Prerequisites</h3>
+
+- Python 3.8.5 ( ​ https://www.python.org/downloads/release/python-380/​ )
+- Text Editor (Atom/Brackets)
+
+<h2> Assumptions </h2>
+1. The input file is in the same folder as the Solution File.
+2. Commands are case-sensitive.
+
+<h3> Repository </h3>
+
+The repository contains the following:
+- Python3 Solution Code
+
+<h3> Learning Outcome </h3>
+
+At the end of this project, we should be able to:
+- solve the Parking Lot problem with data structures optimized for complexity
+- write modular code using Classes
+
+
+<h3> Instructions </h3>
+1. Git clone the project into desired folder in your computer using 
+
+        git clone https://github.com/99shivansh/takehomeassignment.git
+
+
+2. Add a new text file for input by name of your choice in the folder with main.cpp.
+
+3. Microsoft Windows: 
+	
+	
+1.         Open File Explorer and navigate to that same directory.
+	
+2.         While holding down SHIFT, right-click your mouse on the white space avoiding selecting any of the files.
+	
+3.         Select "Open Command Window Here".
+	
+4.        Run the program by typing [python main.py] and follow the instructions via the Command Prompt.
+	
+     
+
+MacOS:
+	
+	
+1.        Open Terminal and navigate to that same directory    - e.g. cd /Users/*User Name*/*Directory Location*/
+	
+2.        Run the program by typing [python3 main.py]  and follow the instructions via the Terminal.
+	
+       
+
+Linux:
+	
+	
+ 1.         Open Terminal and navigate to that same directory - e.g. cd /Users/*User Name*/*Directory Location*/
+	
+ 2.        Run the program by typing [python3 main.py]  and follow the instructions via the Terminal.
+	
+        
+
+<h3> Sample Input </h3>
+
+    Create_parking_lot 6 
+
+    Park KA-01-HH-1234 driver_age 21 
+
+    Park PB-01-HH-1234 driver_age 21
+
+    Slot_numbers_for_driver_of_age 21
+
+    Park PB-01-TG-2341 driver_age 40
+
+    Slot_number_for_car_with_number PB-01-HH-1234
+
+    Leave 2
+
+    Park HR-29-TG-3098 driver_age 39
+
+    Vehicle_registration_number_for_driver_of_age 18
+
+<h3> Sample Output </h3>
+
+ Created parking of 6 slots
+Car with vehicle registration number "KA-01-HH-1234" has been parked at slot number 1
+Car with vehicle registration number "PB-01-HH-1234" has been parked at slot number 2
+1,2
+Car with vehicle registration number "PB-01-TG-2341" has been parked at slot number 3
+2
+Slot number 2 vacated, the car with vehicle registration number "PB-01-HH-1234" left the space, the driver of the car was of age 21
+Car with vehicle registration number "HR-29-TG-3098" has been parked at slot number 2
+
+
+
+
